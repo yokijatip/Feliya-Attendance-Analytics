70bd4822d9667d3be2d9b5bbd14c85ed004aaa5b
Yoki Jati Perkasa <yokijati@gmail.com>
2025-07-19 15:29:24 +0700

    Succes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@pinia/nuxt']
-})
+  modules: ["@nuxt/image", "@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["@nuxt/image", "@pinia/nuxt"],
+    },
+  },
+});
